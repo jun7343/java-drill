f28c37ca2e44e55ca639eeaccdfbccbb0c376e1b
Jun Yu <zlsl7343@gmail.com>
2020-11-18 15:18:17 +0900

    Masulsa Class 추가

---
+++ src/main/java/main/bytecode/Masulsa.java
@@ -0,0 +1,24 @@
+package main.bytecode;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+
+import java.io.File;
+import java.io.IOException;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+public class Masulsa {
+
+    public void toMakeRabbit() {
+        try {
+            new ByteBuddy().redefine(Moja.class)
+                    .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
+                    .make().saveIn(new File("build/classes/java/main/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(new Moja().pullOut());
+    }
+}
